c218e4e662053ab18499faecb0cd6ca13c74ea97
DjafarouAbdou909 <djafarouabdoou@gmail.com>
2026-05-17 14:52:09 +0000

    config: disable debug mode for production environment

--- academic_assistant_api/settings.py
+++ academic_assistant_api/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-fastmemory-gviva-2025")
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
